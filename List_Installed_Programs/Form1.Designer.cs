bbb79f74be19596e551847919989e00b3003f081
Richard Teel <richteel@teelsys.com>
2021-01-31 11:22:02 -0500

    Initial check-in | No further work planned

---
+++ List_Installed_Programs/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace List_Installed_Programs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtProgramsList = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 3;
+            // 
+            // txtProgramsList
+            // 
+            this.txtProgramsList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtProgramsList.Location = new System.Drawing.Point(0, 124);
+            this.txtProgramsList.Name = "txtProgramsList";
+            this.txtProgramsList.ReadOnly = true;
+            this.txtProgramsList.Size = new System.Drawing.Size(800, 304);
+            this.txtProgramsList.TabIndex = 4;
+            this.txtProgramsList.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtProgramsList);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox txtProgramsList;
+    }
+}
+
---
+++ List_Installed_Programs/Form1.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace List_Installed_Programs
+{
+    public partial class Form1 : Form
+    {
+        private readonly DateTime NullDateTime = new DateTime(1970, 1, 1, 0, 0, 0);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //List<SoftwareInfo> programList = new List<SoftwareInfo>();
+            Dictionary<string, SoftwareInfo> programList = new Dictionary<string, SoftwareInfo>();
+
+            txtProgramsList.AppendText("DisplayName\tDisplayVersion\tInstallDateRaw\tInstallDate\tPublisher\tURLInfoAbout\tCount\r\n");
+            GetInstalledPrograms(programList, SoftwareTypes.bit64);
+            GetInstalledPrograms(programList, SoftwareTypes.bit32);
+
+            //programList
+
+            foreach (KeyValuePair<string, SoftwareInfo> softwareInfo in programList.OrderBy(key => key.Key))
+            {
+                txtProgramsList.AppendText(string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\r\n",
+                    softwareInfo.Value.DisplayName,
+                    softwareInfo.Value.DisplayVersion,
+                    softwareInfo.Value.InstallDateRaw,
+                    softwareInfo.Value.InstallDate,
+                    softwareInfo.Value.Publisher,
+                    softwareInfo.Value.URLInfoAbout,
+                    softwareInfo.Value.KeyCount));
+            }
+        }
+
+        private void GetInstalledPrograms(Dictionary<string, SoftwareInfo> ProgramList, SoftwareTypes SoftwareType = SoftwareTypes.bit64)
+        {
+            if (ProgramList == null)
+                return;
+
+            string uninstallKey = "";
+
+            switch (SoftwareType)
+            {
+                case SoftwareTypes.bit32:
+                    uninstallKey = @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
+                    break;
+                case SoftwareTypes.bit64:
+                default:
+                    uninstallKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+                    break;
+            }
+
+            using (RegistryKey rk = Registry.LocalMachine.OpenSubKey(uninstallKey))
+            {
+                foreach (string skName in rk.GetSubKeyNames())
+                {
+                    using (RegistryKey sk = rk.OpenSubKey(skName))
+                    {
+                        SoftwareInfo softwareInfo = GetSoftwareInfoForKey(sk);
+
+                        if (!string.IsNullOrEmpty(softwareInfo.DisplayName))
+                        {
+                            if(ProgramList.Keys.Contains(softwareInfo.DisplayName))
+                            {
+                                SoftwareInfo si = ProgramList[softwareInfo.DisplayName];
+
+                                si.KeyCount++;
+
+                                if (string.IsNullOrEmpty(si.DisplayVersion) && !string.IsNullOrEmpty(softwareInfo.DisplayVersion))
+                                    si.DisplayVersion = softwareInfo.DisplayVersion;
+
+                                if (string.IsNullOrEmpty(si.InstallDateRaw) && !string.IsNullOrEmpty(softwareInfo.InstallDateRaw))
+                                    si.InstallDateRaw = softwareInfo.InstallDateRaw;
+
+                                if (si.InstallDate < softwareInfo.InstallDate)
+                                    si.InstallDate = softwareInfo.InstallDate;
+
+                                if (string.IsNullOrEmpty(si.Publisher) && !string.IsNullOrEmpty(softwareInfo.Publisher))
+                                    si.Publisher = softwareInfo.Publisher;
+
+                                if (string.IsNullOrEmpty(si.URLInfoAbout) && !string.IsNullOrEmpty(softwareInfo.URLInfoAbout))
+                                    si.URLInfoAbout = softwareInfo.URLInfoAbout;
+
+                                ProgramList[softwareInfo.DisplayName] = si;
+                            }
+                            else
+                            {
+                                ProgramList.Add(softwareInfo.DisplayName, softwareInfo);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private SoftwareInfo GetSoftwareInfoForKey(RegistryKey sk)
+        {
+            string DisplayName = "";
+            string DisplayVersion = "";
+            string InstallDateRaw = "";
+            DateTime InstallDate = NullDateTime;
+            string Publisher = "";
+            string URLInfoAbout = "";
+            Int64 InstallDateAsInt = 0;
+
+            if (sk.GetValue("DisplayName") != null)
+                DisplayName = sk.GetValue("DisplayName").ToString().Replace("\0", string.Empty).Trim();
+
+            if (sk.GetValue("DisplayVersion") != null)
+                DisplayVersion = sk.GetValue("DisplayVersion").ToString().Replace("\0", string.Empty).Trim();
+
+            if (sk.GetValue("InstallDate") != null)
+                InstallDateRaw = sk.GetValue("InstallDate").ToString().Replace("\0", string.Empty).Trim();
+
+            if (sk.GetValue("InstallDate") != null && !string.IsNullOrEmpty(sk.GetValue("InstallDate").ToString()))
+            {
+                string s = sk.GetValue("InstallDate").ToString().Replace("\0", string.Empty).Trim();
+
+                if (s.Contains("/"))
+                    DateTime.TryParse(s, out InstallDate);
+                else if(s.Length == 8)
+                {
+                    if (int.TryParse(s.Substring(0, 4), out int year) && int.TryParse(s.Substring(4, 2), out int month) && int.TryParse(s.Substring(6, 2), out int day))
+                        InstallDate = new DateTime(year, month, day);
+                }
+                else if (Int64.TryParse(s, out InstallDateAsInt))
+                    InstallDate = InstallDate.AddSeconds(InstallDateAsInt);
+            }
+
+            if (sk.GetValue("Publisher") != null)
+                Publisher = sk.GetValue("Publisher").ToString().Replace("\0", string.Empty).Trim();
+
+            if (sk.GetValue("URLInfoAbout") != null)
+                URLInfoAbout = sk.GetValue("URLInfoAbout").ToString().Replace("\0", string.Empty).Trim();
+
+
+            return new SoftwareInfo(DisplayName, DisplayVersion, InstallDateRaw, InstallDate, Publisher, URLInfoAbout);
+        }
+    }
+
+    public enum SoftwareTypes
+    {
+        bit32 = 0,
+        bit64 = 1
+    };
+
+    public struct SoftwareInfo
+    {
+        public string DisplayName { get; }
+
+        public string DisplayVersion { get; set; }
+
+        public string InstallDateRaw { get; set; }
+
+        public DateTime InstallDate { get; set; }
+
+        public string Publisher { get; set; }
+
+        public string URLInfoAbout { get; set; }
+
+        public int KeyCount { get; set; }
+
+        public SoftwareInfo(string DisplayName, string DisplayVersion, string InstallDateRaw, DateTime InstallDate, string Publisher, string URLInfoAbout)
+        {
+            this.DisplayName = DisplayName;
+            this.DisplayVersion = DisplayVersion;
+            this.InstallDateRaw = InstallDateRaw;
+            this.InstallDate = InstallDate;
+            this.Publisher = Publisher;
+            this.URLInfoAbout = URLInfoAbout;
+            this.KeyCount = 1;
+        }
+    }
+}
